964b46035b9db1471905fa1ed98030440dc6ac82
v-giorgio <vitorlcardoso17@gmail.com>
2022-04-25 23:14:05 -0300

    bugfix: start animation

--- cinema-rest/src/App.tsx
+++ cinema-rest/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import { GlobalStyle } from "./assets/styles/global";
 import { BrowserRouter } from "react-router-dom";
@@ -6,36 +6,35 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PageRoutes from "./routes";
-import Animation from "./components/Animation/index"
-
+import Animation from "./components/Animation/index";
 
 function App() {
   const [animation, setAnimation] = useState(true);
+
   useEffect(() => {
-    setTimeout(() => {      
-    setAnimation(false)
-    }, 3000)
+    setTimeout(() => {
+      setAnimation(false);
+    }, 3000);
   }, []);
+
   return (
-
     <>
-
       <ChakraProvider>
-        {animation && <Animation />}        
-        <GlobalStyle />
-        <BrowserRouter>
-          <Header />
-
-          <PageRoutes />
-          <Footer />
-        </BrowserRouter>
+        {animation ? (
+          <Animation />
+        ) : (
+          <>
+            <GlobalStyle />
+            <BrowserRouter>
+              <Header />
+              <PageRoutes />
+              <Footer />
+            </BrowserRouter>
+          </>
+        )}
       </ChakraProvider>
-
     </>
-
   );
 }
 
 export default App;
-
-
--- cinema-rest/src/components/Animation/index.tsx
+++ cinema-rest/src/components/Animation/index.tsx
@@ -1,38 +1,35 @@
-import { Container, chakra } from '@chakra-ui/react';
-import { motion, isValidMotionProp } from 'framer-motion';
+import { Container, chakra } from "@chakra-ui/react";
+import { motion, isValidMotionProp } from "framer-motion";
 
 const ChakraBox = chakra(motion.div, {
-
-    shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === "children",
 });
 
 export default function Animation() {
-
-    return (
-        <Container h="100vh" d="flex" alignItems="center" justifyContent="center">
-            <ChakraBox
-                animate={{
-                    scale: [1, 2, 2, 1, 1],
-                    rotate: [0, 0, 270, 270, 0],
-                    borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-                }}
-
-                transition={{
-                    duration: 3,
-                    ease: "easeInOut",
-                    repeat: Infinity,
-                    repeatType: "loop",
-                }}
-                padding="2"
-                bgGradient="linear(to-l, #7928CA, #FF0080)"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                width="100px"
-                height="100px"
-            >
-                Cinerest!
-            </ChakraBox>
-        </Container>
-    )
+  return (
+    <Container h="100vh" d="flex" alignItems="center" justifyContent="center">
+      <ChakraBox
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        }}
+        transition={{
+          duration: 3,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+        padding="2"
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        width="100px"
+        height="100px"
+      >
+        Cinerest!
+      </ChakraBox>
+    </Container>
+  );
 }

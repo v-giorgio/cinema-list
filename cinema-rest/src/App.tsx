3976e2b98025dd414d7069582f50f76a664fd822
Laissa Fernandes <laissafermaria@gmail.com>
2022-04-25 22:30:19 -0300

    feature: criação da animação de abertura

--- cinema-rest/src/App.tsx
+++ cinema-rest/src/App.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
 import { GlobalStyle } from "./assets/styles/global";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PageRoutes from "./routes";
+import Animation from "./components/Animation/index"
+
 
 function App() {
+  const [animation, setAnimation] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {      
+    setAnimation(false)
+    }, 3000)
+  }, []);
   return (
+
     <>
+
       <ChakraProvider>
+        {animation && <Animation />}        
         <GlobalStyle />
         <BrowserRouter>
           <Header />
@@ -19,8 +30,12 @@ function App() {
           <Footer />
         </BrowserRouter>
       </ChakraProvider>
+
     </>
+
   );
 }
 
 export default App;
+
+
